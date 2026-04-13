054683a17f6f21fe2a0fbe36b5462bbc92f112ba
Harshitpant12 <harshpant778@gmail.com>
2026-04-13 13:01:44 +0530

    implement middleware to verify refresh token, export both token verfying middlewares

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -26,3 +26,25 @@ const verifyAccessToken = asyncHandler(async (req, _, next) => {
     req.user = user
     next()
 })
+
+const verifyRefreshToken = asyncHandler(async (req, res, next) => {
+    const token = req.cookies.refreshToken
+    if(!token) throw new ApiError(401, "Unauthorized access")
+
+    let decode;
+    try {
+        decode = jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+    } catch (error) {
+        if(error.name === "TokenExpiredError") throw new ApiError(401, "Unauthorized access") // token expired
+        throw new ApiError(401, "Unauthorized access") // invalid access token
+    }
+
+    const user = await User.findById(decode._id).select("-password")
+    if(!user) throw new ApiError(401, "Unauthorized access") // invalid token
+    if(user.refreshToken !== token) throw new ApiError(401, "Unauthorized access") // invalid token(incorrect token)
+
+    req.user = user
+    next()
+})
+
+export {verifyAccessToken, verifyRefreshToken}
