f3bf994bb86c91066f4ccc826b35650adf03832b
Harshitpant12 <harshpant778@gmail.com>
2026-04-16 19:20:14 +0530

    implement deleteUser functionality for admins to delete normal user with their analyses

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -1,5 +1,6 @@
 import Analysis from "../models/Analysis.js";
 import User from "../models/User.js";
+import ApiError from "../utils/apiError";
 import ApiResponse from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
@@ -12,3 +13,18 @@ const getAllAnalyses = asyncHandler(async(req, res) => {
     const analyses = await Analysis.find().populate("userId", "name email")
     return res.status(200).json(new ApiResponse(200, analyses, "Analyses fetched successfully"))
 })
+
+const deleteUser = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const user = await User.findById(id)
+
+    if(!user) throw new ApiError(404, "User not found")
+    if(user.role === "admin") throw new ApiError(403, "Cannot delete an admin")
+
+    await User.findByIdAndDelete(id)
+    await Analysis.deleteMany({ userId: id }) // also deleting all the analyses by the user
+
+    return res.status(200).json(new ApiResponse(200, null, "User deleted successfully"))
+})
+
+export { getAllUsers, getAllAnalyses, deleteUser }
