d6c1a2798c9608f842053bebc116ce46b57d3bc3
Harshitpant12 <harshpant778@gmail.com>
2026-04-19 09:25:34 +0530

    add responsiveness for hero section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,22 +16,21 @@ function Home() {
                 muted
                 loop
                 playsInline
-                className="absolute top-0 left-0 w-full h-full object-cover hidden md:block opacity-60"
+                className="absolute top-0 left-0 w-full h-full object-cover opacity-60"
                 >
                     <source src={myVideo} type="video/mp4" />
                 </video>
 
-                <div className="absolute top-0 left-0 w-full h-full block md:hidden bg-linear-to-br from-gray-900 via-blue-900 to-gray-900 opacity-80 -z-10"></div>
+                <div className="absolute bottom-0 left-0 w-full h-[65%] bg-linear-to-t from-black via-black/70 to-transparent md:h-full md:bg-none z-10"></div>
+                
 
-                <div className="absolute top-0 left-0 w-full h-full bg-black/40 z-0"></div>
-
-                <div className='absolute max-w-4xl mx-auto left-40 bottom-40'>
+                <div className='absolute z-20 w-full px-6 bottom-20 flex flex-col items-center text-center md:block md:w-auto md:px-0 md:text-left md:max-w-4xl md:mx-auto md:left-40 md:bottom-40'>
                     <motion.h1
                     initial={{opacity: 0, y: 40}}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.5, delay: 0.3 }}
-                    className='text-white text-7xl my-4'>Beat the ATS. <br /> Land the interview</motion.h1>
-                    <Link to='/register' className='inline-block rounded-4xl text-xl bg-white hover:bg-white/80 text-black px-6 py-5 mt-10'>
+                    className='text-white text-4xl md:text-7xl my-4'>Beat the ATS. <br /> Land the interview</motion.h1>
+                    <Link to='/register' className='inline-block rounded-4xl text-xl bg-white hover:bg-white/80 text-black px-6 py-5 mt-6 md:mt-10'>
                         Start Free Analysis
                     </Link>
                 </div>
